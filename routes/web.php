9c13e73696f1b3082904c1875b410c58c21563f3
oshiumitsuyoshi <ot104752gmail.com>
2025-01-19 11:29:06 +0900

    プロジェクト準備完了

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function (){
+    return view('welcome');
+});
+
+Route::resource('memos', MemoController::class);
