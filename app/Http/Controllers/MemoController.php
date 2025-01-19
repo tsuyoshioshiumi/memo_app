75ce9807c3407295d0cd1ded1e53f93046790976
oshiumitsuyoshi <ot104752gmail.com>
2025-01-19 12:12:20 +0900

    照会機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memo;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
 }
