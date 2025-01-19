b93bca348327e4133e3f454bb6e6e1b8cb9e5127
oshiumitsuyoshi <ot104752gmail.com>
2025-01-19 14:00:53 +0900

    一覧機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
+    public function index()
+    {
+        // 全県取得
+        $memos = Memo::all();
+
+        return view('memos.index',['memos' => $memos]);
+    }
     public function show($id)
     {
         $memo = Memo::find($id);
