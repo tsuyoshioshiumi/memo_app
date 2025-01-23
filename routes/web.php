6eee0fcdb467026338543132a3c3ed9b6174899f
oshiumitsuyoshi <ot104752gmail.com>
2025-01-23 10:42:15 +0900

    Renderデプロイのために設定

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function (){
-    return view('welcome');
-});
+Route::get('/', [MemoController::class, 'index']);
+
+
+
 
 Route::resource('memos', MemoController::class);
